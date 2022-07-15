a0ff926feafd46d497021627059fbc259e6e72c1
lks2007 <lukas.soigneux@gmail.com>
2022-07-15 17:22:44 +0200

    add object storage coockie

--- src/main.js
+++ src/main.js
@@ -17,3 +17,30 @@ $(".choice").click(function() {
     $(".active").removeClass("active")
     $(this).addClass("active")
 })
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function set_cookie(cookiename, cookievalue, hours) {
+    var date = new Date();
+    date.setTime(date.getTime() + Number(hours) * 3600 * 1000);
+    document.cookie = cookiename + "=" + cookievalue + "; path=/;expires = " + date.toGMTString();
+
+}
+
+$(".small").click(function() {
+    set_cookie("object", getCookie("object")+"|"+$(this).attr("aria-label"), 24*365);
+})
