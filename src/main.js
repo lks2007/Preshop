92a224ee715d6bd1080963987db0b9fd64663ce9
lks2007 <lukas.soigneux@gmail.com>
2022-07-15 22:09:05 +0200

    add profile preview

--- src/main.js
+++ src/main.js
@@ -47,3 +47,83 @@ $(".small").click(function() {
 
     $(".round").text(result)
 })
+
+
+if($(".profile").length){
+    var a = 0;
+}
+
+if($(".modal").length){
+    var i = 0;
+    $(".modal").click(function() {
+        if(a === 1){
+            $(".moda").remove()
+            a = 0;
+        }
+
+        if(i === 0){
+            $("body").append(`
+                <div class="mod">
+                    <a class="b-box" href="/dashboard.php">
+                        <div class="box">
+                            <i class="fas fa-chart-line"></i>
+                            <p>Dashboard</p>
+                        </div>
+                    </a>
+                    <a class="b-box" href="/inbox.php">
+                        <div class="box__">
+                            <i class="fas fa-inbox"></i>
+                            <p>Inbox</p>
+                        </div>
+                    </a>
+                    <a class="b-box" href="/setting.php">
+                        <div class="box__">
+                            <i class="fas fa-cog"></i>
+                            <p>Settings</p>
+                        </div>
+                    </a>
+                </div>
+            `)
+            i = 1;
+        }else{
+            $(".mod").remove()
+            i = 0;
+        }
+    })
+}
+
+if($(".profile").length){
+    var a = 0;
+    $(".profile").click(function() {
+        if(i === 1){
+            $(".mod").remove()
+            i = 0;
+        }
+
+        if(a === 0){
+            $("body").append(`
+                <div class="moda">
+                    <img class="btn-3 mr-0" src="https://icebeal.herokuapp.com/api/identicon/lksdev.svg" />
+                    <p class="name">lksdev</p>
+                    <p class="email">lukas.soigneux@gmail.com</p>
+                    <a class="b-box" href="/account/">
+                        <div class="box__">
+                            <i class="fas fa-cog"></i>
+                            <p>Account Settings</p>
+                        </div>
+                    </a>
+                    <a class="b-box" href="/logout/">
+                        <div class="box__">
+                            <i class="fas fa-arrow-right"></i>
+                            <p>Logout</p>
+                        </div>
+                    </a>
+                </div>
+            `)
+            a = 1;
+        }else{
+            $(".moda").remove()
+            a = 0;
+        }
+    })
+}
