9b58e4595c4d76c36c1f89fd7922fddfe3995209
lks2007 <lukas.soigneux@gmail.com>
2022-07-15 20:09:46 +0200

    add shop list view

--- src/main.js
+++ src/main.js
@@ -18,29 +18,32 @@ $(".choice").click(function() {
     $(this).addClass("active")
 })
 
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArr = cDecoded.split('; ');
+    let res;
+    cArr.forEach(val => {
+      if (val.indexOf(name) === 0) res = val.substring(name.length);
+      toString(res)
+    })
+    return res
+  }
 
 function set_cookie(cookiename, cookievalue, hours) {
     var date = new Date();
     date.setTime(date.getTime() + Number(hours) * 3600 * 1000);
-    document.cookie = cookiename + "=" + cookievalue + "; path=/;expires = " + date.toGMTString();
-
+    document.cookie = cookiename + "=" + cookievalue + "; expires = " + date.toGMTString();
 }
+var result = getCookie("object").split("|").length-1;
+
+$(".round").text(result)
 
 $(".small").click(function() {
     set_cookie("object", getCookie("object")+"|"+$(this).attr("aria-label"), 24*365);
+    var object = getCookie("object")
+
+    var result = getCookie("object").split("|").length-1;
+
+    $(".round").text(result)
 })
